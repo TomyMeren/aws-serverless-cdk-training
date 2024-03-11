f0158bd0ea657cc88f5e44f7f9ec5d3e7794502b
Tomas Merendon <tomas.merendon@gmail.com>
2024-03-11 17:31:15 +0100

    Initial commit

---
+++ test/xebia_serverles_cdk_training.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as XebiaServerlesCdkTraining from '../lib/xebia_serverles_cdk_training-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/xebia_serverles_cdk_training-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new XebiaServerlesCdkTraining.XebiaServerlesCdkTrainingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
