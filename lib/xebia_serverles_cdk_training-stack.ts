3b7dd62a16e147d392573fb525d185f46cde7b4e
Tomas Merendon <tomas.merendon@gmail.com>
2024-03-13 16:55:45 +0100

    the lambda print the content of the file in a event

--- lib/xebia_serverles_cdk_training-stack.ts
+++ lib/xebia_serverles_cdk_training-stack.ts
@@ -1,7 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { aws_s3 as s3 } from 'aws-cdk-lib';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { S3EventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class XebiaServerlesCdkTrainingStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -12,11 +14,17 @@ export class XebiaServerlesCdkTrainingStack extends cdk.Stack {
       autoDeleteObjects: true
     });
 
-    const lambdaA = new lambda.Function(this, 'lambda-A', {
-      runtime: lambda.Runtime.NODEJS_16_X ,
-      code: lambda.Code.fromAsset('lambda'),
-      handler: 'lambdaA.handler'      
+    const lambdaA = new NodejsFunction (this, 'lambdaA', {
+      entry: './lambda/lambdaA.ts',
     });
+
+    lambdaA.addToRolePolicy(new PolicyStatement({
+      sid: "permisionToObjectInsideBucket",
+      resources:[rawBucket.arnForObjects('*')],
+      effect: Effect.ALLOW,
+      actions: ["s3:GetObject"]
+    })
+    );
 
     const source = new S3EventSource(rawBucket, {
       events: [s3.EventType.OBJECT_CREATED]
